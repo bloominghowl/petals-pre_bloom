4a91258a8a25c4a620435c418c0484c541a0c84b
Ayivi Angela <104152275+bloominghowl@users.noreply.github.com>
2023-10-04 02:27:33 +0000

    Create 2-DogYears.js

---
+++ Codecademy_Closet/JS_Jungle/2-DogYears.js
@@ -0,0 +1,24 @@
+//Declaring variable myAge
+const myAge = 5;
+
+//Declaring variable earlyYears
+let earlyYears = 2;
+
+earlyYears = earlyYears * 10.5;
+
+//Deducting 2 from myAge and assigning 
+//result to Variable laterYears
+let laterYears = myAge - 2;
+
+//calculating the number of dog years accounted for
+laterYears *= 4;
+
+//Assigning the addition of variables laterYears and
+//earlyYears to myAgeInDogYears
+let myAgeInDogYears = earlyYears + laterYears;
+
+//Storing my name as a string to variable myName
+let myName = `Blooming Howl`.toLowerCase();
+
+//Introducing myself
+console.log(`My name is ${myName}, I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
